1ee0433a811c76b258fb7304254188bc91b346f1
sreejit <sreejitkundu86537@gmail.com>
2024-01-31 12:36:11 +0530

    First commit

---
+++ movie/apps/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+def book(request):
+    return render(request, 'book.html')
